fbc8fbdf5027339c1fb4645173a1f4e16d801d8c
mcr222 <markitus222@hotmail.com>
2017-11-10 13:14:56 +0100

    Added dataset and prints. Run with: python main.py 0.3 (0.3 any double)

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import random
 from sklearn.metrics import jaccard_similarity_score
 import scipy.optimize as sp
 import numpy as np
+import sys
         
 def shingle_text(text, k):
     '''
     Takes a text and partitions it in hashed (4 byte hash) shingles of length k.
         @param text: string containing the text to shingle
-        @param k: length of the characters in a shingle
+        @param k: number of characters in a shingle
         @return: ordered (by input order) set containing all hashes.
     '''
     shingled_text = SortedSet()
@@ -25,7 +26,7 @@ def shingle_text(text, k):
 
 def compareSets(set1,set2):
     '''
-    Returns Jaccard similarity index between the two sets
+    Returns Jaccard similarity index between the two sets.
         @param set1: first set
         @param set2: second set
         @return: ratio of items in the intersection of the sets vs all distinct items in both sets
@@ -34,7 +35,7 @@ def compareSets(set1,set2):
 
 def compareSignatures(sig1,sig2):
     '''
-    Compares two arrays of integers (same length) considering how many integers are equal per position
+    Compares two arrays of integers (same length) considering how many integers are equal per position.
         @param sig1: array of integers with signature
         @param sig2: other signature to compare
         @return: average of positions in arrays that are equal in both signatures
@@ -45,25 +46,33 @@ def compareSignatures(sig1,sig2):
 
 def minHashing(allSets):
     '''
-        Builds a minhash signature for all the entry sets.
-            @param allSets - a set of document's shingle sets
-            @return: a matrix containing the minhash signature of each document (each column represents one document).
+    Builds a minhash signature for all the entry sets, with each signature containing 100 integers.   
+        @param allSets - a set of document's shingle sets
+        @return: a matrix containing the minhash signature of each document (each column represents one document).
     '''
 
-    #Union of all the hashes
+    print "Computing MinHash"
+    #Union of all the shingle hashes that we have in the sets
     U = SortedSet()
     for i in range(0,len(allSets)):
         U |= allSets[i]
 
    #Generation of 100 hash functions
     totalFunctions = 100
-    div = 10967819 #Prime number bigger than 1'000.000. Other options: 2724299, 3770953, 4311301.
+    #to avoid collisions we make the number of buckets in the hash (determined by mod) very big.
+    #Ideally, we would make this number larger than 2^32 (number of shingle hashes), but in order to
+    #have a reasonable bucket list in LSH function without collision we reduced this modulus number.
+    mod = 2971215073
+    #we also choose high parameters for a and b for the modulus in order to avoid having a hash function
+    #that does not permute the rows (equivalent to identity function) 
+    low = 1000
+    high = 1000000
     functions = []
     for i in range(0,totalFunctions):
         def func1(x):
-            a = random.randint(1,totalFunctions)
-            b = random.randint(1,totalFunctions)
-            return (a*x +b)%div
+            a = random.randint(low,high)
+            b = random.randint(low,high)
+            return (a*x +b)%mod
         functions.append(func1)
 
    #Min hash algorithm
@@ -87,11 +96,11 @@ def minHashing(allSets):
                         M_i_c[hh][c_cont] = h_r[hh]
             c_cont+=1
         i+=1
-
-    print M_i_c
+    
+    print "Finished computing MinHash"
     return M_i_c
 
-def LHS(signature_vectors, threshold):
+def LSH(signature_vectors, threshold):
     '''
     Locality sensitive hashing, efficiently computes all pairs of documents that are, with high probability,
     over (or very close) the similarity threshold determined. Candidates are selected
@@ -100,13 +109,13 @@ def LHS(signature_vectors, threshold):
                 (there can be false positives, i.e. candidate pairs that their similarity is not over threshold).
         @return: a set with all pairs of documents that are candidate to be similar with threshold determined
     '''
+    print "Computing LSH"
     sign_length = len(signature_vectors[0])
-    print "signature length: " + str(sign_length)
     print "threshold required: " + str(threshold)
     root_res = sp.root(lambda r: (r/sign_length)**(1/r)-threshold, 2)
     #beware that r might not divide exactly sign_length (last band might be smaller than r)
     r = int(root_res['x'][0])
-    print "number of rows: " + str(r)
+    print "number of rows in a band of LSH: " + str(r)
     candidate_pairs = set()
     start_idx = 0
     end_idx = r
@@ -143,31 +152,54 @@ def LHS(signature_vectors, threshold):
         if(end_idx> sign_length):
             end_idx = sign_length
 
-
-    print candidate_pairs
+    print "Finished computing LSH"
     return candidate_pairs
 
 
-def main():
-    text1="try text"
-    text2="hello text"
-    k=3
-    set1 = shingle_text(text1, k)
-    print set1
-    set2 = shingle_text(text2, k)
-    print set2
-    print compareSets(set1, set2)
-    print compareSignatures([2,2,2], [2,4,2])
-    #we transpose to have the signatures of each document in a single array
-    signature_matrix = np.array(minHashing([set1,set2])).T
-    print signature_matrix
-    print "number of documents: " + str(len(signature_matrix))
-    print "number of hashes in each signature: " + str(len(signature_matrix[0]))
-    print "signature similarity: " + str(compareSignatures(signature_matrix[0], signature_matrix[1]))
-    LHS(signature_matrix, 0.5)
-
+def main(t=0.8):
+    #we set the shingle length to most used value for documents
+    k=10
     
-main()
+    with open("dataset.txt") as dataset:
+        allSets = []
+        for line in dataset:
+            allSets.append(shingle_text(line, k))
+        
+        print "number of documents: " + str(len(allSets))
+        print "Lenght of shingled set for first document:"
+        print len(allSets[0])
+        print "Jaccard Similarity between sets 0 and 1 (representing documents 0 and 1)"
+        print compareSets(allSets[0], allSets[1])
+        print "Jaccard Similarity between sets 0 and 6 (they are the same text with some sentences missing on 6)"
+        print compareSets(allSets[0], allSets[6])
+        print "Jaccard Similarity between sets 0 and 14 (14 is half the text of 0)"
+        print compareSets(allSets[0], allSets[14])
+        
+        #we apply minHashing to all sets to obtain each document's signature. 
+        #we transpose the matrix to have the signatures of each document in a single array.
+        signature_matrix = np.array(minHashing(allSets)).T
+        
+        print "number of hashes in each signature: " + str(len(signature_matrix[0]))
+        print "Similarity between signatures of 0 and 1"
+        print compareSignatures(signature_matrix[0], signature_matrix[1])
+        print "Similarity between signatures 0 and 6"
+        print compareSignatures(signature_matrix[0], signature_matrix[6])
+        print "Similarity between signatures 0 and 14"
+        print compareSignatures(signature_matrix[0], signature_matrix[14])
+        
+        candidate_pairs = LSH(signature_matrix, t)
+        print "Pairs that are candidates to be similar with threshold " + str(t)
+        print candidate_pairs
+        similar_pairs = []
+        for pair in candidate_pairs:
+            if(compareSignatures(signature_matrix[pair[0]], signature_matrix[pair[1]])):
+                similar_pairs.append(pair)
+                
+        print "Pairs that are similar with threshold " + str(t) + " (although there can be some false negatives and positives)"
+        print similar_pairs
+
+
+main(float(sys.argv[1]))
 
 
 
